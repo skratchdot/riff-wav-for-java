8b6275ab71c95199eb44a3aabe472a2caad86cf0
skratchdot <git@skratchdot.com>
2010-02-11 21:07:05 +0000

    First/untested implementation of the WavRandomAccessFile class. This should read/write in little endian format. Also has helpers for UnsignedShort and UnsignedInt
    git-svn-id: http://riff-wav-for-java.googlecode.com/svn/trunk@14 9faeb28e-1408-11df-bbb9-3d7b7cea6d78

---
+++ com.skratchdot.riff.wav/src/com/skratchdot/riff/wav/util/WavRandomAccessFile.java
@@ -0,0 +1,199 @@
+package com.skratchdot.riff.wav.util;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/**
+ * A RandomAccessFile wrapper that converts data to Little Endian format.
+ * @author jeff@skratchdot.com
+ */
+public final class WavRandomAccessFile implements DataInput, DataOutput {
+	private final RandomAccessFile raf;
+
+	public WavRandomAccessFile(File file, String mode) throws FileNotFoundException {
+		this.raf = new RandomAccessFile(file, mode);
+	}
+
+	public WavRandomAccessFile(String name, String mode) throws FileNotFoundException {
+		this.raf = new RandomAccessFile(name, mode);
+	}
+
+	@Override
+	public boolean readBoolean() throws IOException {
+		return raf.readBoolean();
+	}
+
+	@Override
+	public byte readByte() throws IOException {
+		return raf.readByte();
+	}
+
+	@Override
+	public char readChar() throws IOException {
+		return Character.reverseBytes(raf.readChar());
+	}
+
+	@Override
+	public double readDouble() throws IOException {
+		return Double.longBitsToDouble(Long.reverseBytes(raf.readLong()));
+	}
+
+	@Override
+	public float readFloat() throws IOException {
+		return Float.intBitsToFloat(Integer.reverseBytes(raf.readInt()));
+	}
+
+	@Override
+	public void readFully(byte[] b) throws IOException {
+		// TODO: Doesn't convert to Little Endian
+		raf.readFully(b);		
+	}
+
+	@Override
+	public void readFully(byte[] b, int off, int len) throws IOException {
+		// TODO: Doesn't convert to Little Endian
+		raf.readFully(b, off, len);
+	}
+
+	@Override
+	public int readInt() throws IOException {
+		return Integer.reverseBytes(raf.readInt());
+	}
+
+	@Override
+	public String readLine() throws IOException {
+		// TODO: Doesn't convert to Little Endian
+		return raf.readLine();
+	}
+
+	@Override
+	public long readLong() throws IOException {
+		return Long.reverseBytes(raf.readLong());
+	}
+
+	@Override
+	public short readShort() throws IOException {
+		return Short.reverseBytes(raf.readShort());
+	}
+
+	@Override
+	public String readUTF() throws IOException {
+		// TODO: Doesn't convert to Little Endian
+		return raf.readUTF();
+	}
+
+	@Override
+	public int readUnsignedByte() throws IOException {
+		return (int) this.readByte();
+	}
+
+	@Override
+	public int readUnsignedShort() throws IOException {
+		/*
+		int i = (int) this.readShort();
+		return i<0?i+Short.MAX_VALUE:i;
+		*/
+		return (int) this.readShort();
+	}
+	
+	public long readUnsignedInt() throws IOException {
+		return (long) this.readInt();
+	}
+
+	@Override
+	public int skipBytes(int n) throws IOException {
+		return raf.skipBytes(n);
+	}
+
+	@Override
+	public void write(int b) throws IOException {
+		raf.write(b);
+	}
+
+	@Override
+	public void write(byte[] b) throws IOException {
+		raf.write(b);
+	}
+
+	@Override
+	public void write(byte[] b, int off, int len) throws IOException {
+		raf.write(b, off, len);
+	}
+
+	@Override
+	public void writeBoolean(boolean v) throws IOException {
+		raf.writeBoolean(v);
+	}
+
+	@Override
+	public void writeByte(int v) throws IOException {
+		raf.writeByte(v);
+	}
+
+	@Override
+	public void writeBytes(String s) throws IOException {
+		raf.writeBytes(s);
+	}
+
+	@Override
+	public void writeChar(int v) throws IOException {
+		raf.writeChar(Integer.reverseBytes(v));
+	}
+
+	@Override
+	public void writeChars(String s) throws IOException {
+		raf.writeChars(s);
+	}
+
+	@Override
+	public void writeDouble(double v) throws IOException {
+		long l = Double.doubleToLongBits(v);
+		double d = Double.longBitsToDouble(Long.reverseBytes(l));
+		raf.writeDouble(d);
+	}
+
+	@Override
+	public void writeFloat(float v) throws IOException {
+		int i = Float.floatToIntBits(v);
+		float f = Float.intBitsToFloat(Integer.reverseBytes(i));
+		raf.writeFloat(f);
+	}
+
+	@Override
+	public void writeInt(int v) throws IOException {
+		raf.writeInt(Integer.reverseBytes(v));
+	}
+
+	@Override
+	public void writeLong(long v) throws IOException {
+		raf.writeLong(Long.reverseBytes(v));
+	}
+
+	@Override
+	public void writeShort(int v) throws IOException {
+		raf.writeShort(Integer.reverseBytes(v));
+	}
+
+	public void writeShort(short v) throws IOException {
+		raf.writeShort(Short.reverseBytes(v));
+	}
+
+	public void writeUnsignedShort(int v) throws IOException {
+		raf.writeShort(Integer.reverseBytes(v));
+	}
+	
+	public void writeUnsignedInt(long v) throws IOException {
+		raf.writeInt((int) Long.reverseBytes(v));
+	}
+
+	@Override
+	public void writeUTF(String s) throws IOException {
+		raf.writeUTF(s);
+	}
+
+	
+}
